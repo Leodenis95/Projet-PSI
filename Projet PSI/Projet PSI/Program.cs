f6a20e57d32f22822c9a6b7604b07516a549428b
Leo <bmdxj7r57y@privaterelay.appleid.com>
2026-01-14 14:21:28 +0100

    Add files via upload

---
+++ Projet PSI/Projet PSI/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Projet_PSI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("");
+
+            int n = 0;
+
+            Console.WriteLine(n);
+
+        }
+    }
+}
